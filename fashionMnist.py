3882aa746e3e13bfd230263dd291fece71b74801
Rohith04MVK <007rohitjj04@gmail.com>
2020-11-13 08:20:24 +0530

    Formatted the document.

--- fashionMnist.py
+++ fashionMnist.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # get the data
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets\
-.fashion_mnist.load_data()
+    .fashion_mnist.load_data()
 
 class_names = ['T-shirt', 'trousers', 'pullover', 'dress', 'coat',
                'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
@@ -36,15 +36,18 @@ print("Test accuracy: ", test_acc)
 
 model.save('model.h5')'''
 
+
 def get_image(path):
     img = cv2.imread(path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return cv2.resize(img, (28,28))
+    return cv2.resize(img, (28, 28))
 
-images = np.array([get_image("./images/trousers.png"), get_image("./images/ankleboot.png"), get_image('./images/coat.png')])
-plt.figure(figsize=(10,10))
+
+images = np.array([get_image("./images/trousers.png"),
+                   get_image("./images/ankleboot.png"), get_image('./images/coat.png')])
+plt.figure(figsize=(10, 10))
 for i in range(3):
-    plt.subplot(1,3,i+1)
+    plt.subplot(1, 3, i+1)
     plt.xticks([])
     plt.yticks([])
     plt.imshow(images[i], cmap=plt.cm.binary)
@@ -56,25 +59,28 @@ images_reshaped = tf.cast(images_reshaped, tf.float32)
 model = tf.keras.models.load_model("model.h5")
 preds = model.predict(images_reshaped)
 
+
 def plot_image(prediction, img):
     plt.xticks([])
     plt.yticks([])
     plt.imshow(img, cmap=plt.cm.binary)
     predicted_label = np.argmax(prediction)
     plt.xlabel("{} {:2.0f}%".format(class_names[predicted_label],
-               100*np.max(prediction),
-               ),
-                color="blue")
-    
+                                    100*np.max(prediction),
+                                    ),
+               color="blue")
+
+
 def plot_value_array(prediction):
     plt.xticks(range(10))
     plt.yticks([])
     thisplot = plt.bar(range(10), prediction, color="#888888")
-    plt.ylim([0,1])
+    plt.ylim([0, 1])
     predicted_label = np.argmax(prediction)
     thisplot[predicted_label].set_color('blue')
-    
-plt.figure(figsize=(8,12))
+
+
+plt.figure(figsize=(8, 12))
 for i in range(3):
     # image
     plt.subplot(3, 2, 2*i+1)
@@ -82,4 +88,4 @@ for i in range(3):
     # bar chart
     plt.subplot(3, 2, 2*i+2)
     plot_value_array(preds[i])
-plt.show() 
+plt.show()
