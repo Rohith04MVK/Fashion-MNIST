9445b3b9c87d951103d401e6813ac678258c1b9a
Rohith04MVK <007rohitjj04@gmail.com>
2020-11-16 19:21:44 +0530

    Split the single file to multiple ones

--- makePredictions.py
+++ makePredictions.py
@@ -3,40 +3,9 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-# get the data
-(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets\
-    .fashion_mnist.load_data()
-
 class_names = ['T-shirt', 'trousers', 'pullover', 'dress', 'coat',
                'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
 
-
-# reshaping
-'''train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
-test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
-
-# define the model
-model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28,28,1)),
-        tf.keras.layers.MaxPooling2D(2,2),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(10, activation='softmax')
-        ])
-# sample output: [0, 0.1, 0, 0.2, 0.7, 0, 0, 0, 0, 0] --> coat
-    
-model.compile(optimizer='adam', loss="sparse_categorical_crossentropy", 
-              metrics=['accuracy'])
-
-# train
-model.fit(train_images, train_labels, epochs=10)
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-
-print("Test accuracy: ", test_acc)
-
-model.save('model.h5')'''
-
-# predicting images
 
 def get_image(path):
     img = cv2.imread(path)
@@ -87,11 +56,3 @@ for i in range(3):
     plt.subplot(3, 2, 2*i+2)
     plot_value_array(preds[i])
 plt.show()
-
-
-'''# Ploting train data
-for i in range(10):
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.title(f"{class_names[train_labels[i]]}")
-    plt.text(0.01, 0.5,f'{train_images[i].shape}')
-    plt.show()'''
