578c19314719744e80b38f36ce66d2afa349ca8b
Rohith04MVK <007rohitjj04@gmail.com>
2020-11-20 07:02:40 +0530

    Bug Fixes

--- imageSorter.py
+++ imageSorter.py
@@ -19,23 +19,19 @@ def get_image(path):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return cv2.resize(img, (28, 28))
 
-#model = tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model("model.h5")
 mypath = "./images/{}"
 path = "./images/"
 onlyfiles = [file for file in listdir(path) if isfile(join(path, file))]
 
 for i in range(len(onlyfiles)):
+    images = np.array([get_image(f"./images/{onlyfiles[i]}")])
+    images_reshaped = images.reshape(images.shape[0], 28, 28, 1)
+    images_reshaped = tf.cast(images_reshaped, tf.float32)
+    preds = model.predict(images_reshaped)
+    image_type = class_names[np.argmax(preds)]
+
     if isdir(mypath.format(onlyfiles[i].split('.') [0])):
-        shutil.move(f'./images/{onlyfiles[i]}', f"./images/{onlyfiles[i].split('.') [0]}")
+        shutil.move(f'./images/{onlyfiles[i]}', f"./images/{image_type}")
     else:
         print("There is no folder for this")
-
-images_reshaped = images.reshape(images.shape[0], 28, 28, 1)
-images_reshaped = tf.cast(images_reshaped, tf.float32)
-
-
-preds = model.predict(images_reshaped)
-
-image_type = class_names[np.argmax(preds)]
-
-print(image_type)
